e96def3cf957d625be659d00021e24eca69907de
LakeFishing <66842683+LakeFishing@users.noreply.github.com>
2022-03-23 14:03:14 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,48 @@
+# a = int(input())
+# b = int(input())
+# total = 0
+
+# if a % 2 != 0:
+#     a += 1
+# if b % 2 == 0:
+#     b += 1
+
+# for i in range(a, b, 2):
+#     total += i
+
+# print(total)
+
+# a = int(input())
+
+# for i in range(0, a):
+#     for j in range(0, a):
+#         print(i+1, '*',  j+1 , '=', (i+1)*(j+1))
+#     print('\n')
+
+
+# a = int(input())
+# li = []
+
+# while a != 9999:
+#     li.append(a)
+#     a = int(input())
+
+# print(min(li))
+
+# a = int(input())
+# b = int(input())
+# li = []
+
+# for i in range(a, b + 1):
+#     if i % 4 == 0:
+#         li.append(i)
+#     elif i % 9 == 0:
+#         li.append(i)
+
+# print(li)
+# print(len(li))
+# # len(字串, 清單, ...)
+# total = 0
+# for j in li:
+#     total += j
+# print(total)
