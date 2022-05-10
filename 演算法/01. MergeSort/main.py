e8ad7861bfb727cec5ab8ab157b8144f66232fdd
LakeFishing <66842683+LakeFishing@users.noreply.github.com>
2022-05-10 23:13:05 +0800

    20220510

---
+++ 演算法/01. MergeSort/main.py
@@ -0,0 +1,46 @@
+import math
+
+def merge(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    
+    L = [None] * (n1 + 1)
+    R = [None] * (n2 + 1)
+    
+    for i in range(n1):
+        L[i] = A[p + i]
+    for j in range(n2):
+        R[j] = A[q + j + 1]
+
+    L[n1] = math.inf
+    R[n2] = math.inf
+
+    i = 0
+    j = 0
+    k = p
+
+    for m in range(k, r + 1):
+        if L[i] <= R[j]:
+            A[m] = L[i]
+            i += 1
+        else:
+            A[m] = R[j]
+            j += 1
+
+def mergeSort(A, p, r):
+    if p < r:
+        q = int((p + r) // 2)
+        mergeSort(A, p, q)
+        print('%s' %(A[p : q + 1]))
+        mergeSort(A, q + 1, r)
+        print('%s' %(A[q + 1 : r + 1]))
+        merge(A, p, q, r)
+        
+
+count = int(input('共幾個數字？'))
+quizList = []
+for i in range(count):
+    quizList.append(int(input('第 %d 個數字：' %(i + 1))))
+
+mergeSort(quizList, 0, count - 1)
+print('結果：%s' %(quizList))
